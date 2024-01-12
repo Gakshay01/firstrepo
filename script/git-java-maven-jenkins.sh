4aa997dcbcfb33231dac39b16a76458c41436943
Gakshay01 <137770461+Gakshay01@users.noreply.github.com>
2024-01-12 20:32:46 +0530

    Update git-java-maven-jenkins.sh
    Changes has been done to check poll scm

--- script/git-java-maven-jenkins.sh
+++ script/git-java-maven-jenkins.sh
@@ -14,6 +14,8 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
+sudo systemctl jenkins status 
+
 sudo apt update
 
 sudo apt -y install jenkins
