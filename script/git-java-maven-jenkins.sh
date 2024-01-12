83e27640beeb9b605af2328e81dab8195914b6da
Gakshay01 <137770461+Gakshay01@users.noreply.github.com>
2024-01-12 20:37:31 +0530

    Update git-java-maven-jenkins.sh
    to check poll scm

--- script/git-java-maven-jenkins.sh
+++ script/git-java-maven-jenkins.sh
@@ -14,8 +14,8 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-sudo systemctl jenkins status 
-
+sudo systemctl status jenkins 
+sudo systemctl enable jenkins 
 sudo apt update
 
 sudo apt -y install jenkins
